cab5238e80e9673c7abcef52d6236fd48e9860a3
DHLSmith <damianhlsmith@gmail.com>
2024-04-17 22:46:34 +0100

    added english comments

--- taking_float.py
+++ taking_float.py
@@ -1,6 +1,7 @@
 import re
 
 # 打开文件
+# open document
 with open('data.txt', 'r', encoding='utf-8') as file:
     content = file.read()
 
@@ -10,6 +11,7 @@ results = re.findall(r"Test results wb\n\{[^\}]+\}", content)
 # 遍历每个结果块
 for result in results:
     # 在当前结果块中查找 "worst_accuracy" 后的浮点数
+    # look in results for the floating point after "worst accuracy"
     match = re.search(r"'worst_accuracy': (\d+\.\d+)", result)
     if match:
         print(match.group(1))
